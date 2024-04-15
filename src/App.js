3a978f8b3a3c2eb5cf51c3f0ba3c0d8377c7a539
Pinto Manuel <pintoydc@gmail.com>
2024-04-15 13:33:14 +0200

    Project setup completed and next ui and tailwind css is installed

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Button } from "@nextui-org/react";
+
+function App() {
+  return <div className="flex gap-4 items-center">
+    <Button color="primary">
+      Button
+    </Button>
+  </div>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const {nextui} = require("@nextui-org/theme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./node_modules/@nextui-org/theme/dist/components/button.js",
+    './node_modules/@nextui-org/theme/dist/components/(button|snippet|code|input).js',
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+};
